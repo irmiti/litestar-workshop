b775b127bd23a040d993c458315e571bca84bda9
irmiti <miaskad@gmail.com>
2025-10-30 16:56:26 +0100

    Add logging

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from litestar import Litestar, get
+from litestar import Litestar, get, Request
 from litestar.exceptions import ValidationException
+from litestar.logging import LoggingConfig
 from litestar.status_codes import HTTP_500_INTERNAL_SERVER_ERROR
 
 from controllers.param_demo import ParamDemoController
@@ -9,8 +10,18 @@ import error_handler
 
 
 @get("/")
-async def index() -> str:
+async def index(request: Request) -> str:
+    request.logger.info("Nice log message")
     return "Hello, Litestar!"
+
+
+logging_config = LoggingConfig(
+    root={"level": "INFO", "handlers": ["queue_listener"]},
+    formatters={
+        "standard": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+    },
+    log_exceptions="always",
+)
 
 
 app = Litestar(
@@ -19,5 +30,6 @@ app = Litestar(
         ValueError: error_handler.value_error_handler,
         ValidationException: error_handler.validation_exception_handler,
         HTTP_500_INTERNAL_SERVER_ERROR: error_handler.internal_server_error_handler,
-    }
+    },
+    logging_config=logging_config,
 )
